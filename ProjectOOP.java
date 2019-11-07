ece968357675044331785000fdce15dd8ebee770
Cringo01 <51794611+Cringo01@users.noreply.github.com>
2019-11-08 00:52:26 +0800

    Add files via upload

---
+++ ProjectOOP.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectoop;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Joshua
+ */
+public class ProjectOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        try {
+            Login lg = new Login();
+            Signup su = new Signup();
+            BudTrac bt = new BudTrac();
+            
+            mt.getClass();
+            
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ProjectOOP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
